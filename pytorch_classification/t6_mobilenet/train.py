4cc5bea2a9f0a1eb5dae5113eade8085f56d4a68
albert <albertchen.dev@outlook.com>
2022-08-06 15:55:34 +0800

    增加mobilenet v2模型

---
+++ pytorch_classification/t6_mobilenet/train.py
@@ -0,0 +1,58 @@
+import platform
+
+import torch
+from pytorch_lightning import Trainer
+from torch.utils.data import DataLoader
+from torchsummary import summary
+from torchvision import datasets
+from torchvision.transforms import transforms
+
+from pytorch_classification.t6_mobilenet.model import MobileNetV2
+
+
+def test():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = MobileNetV2()
+    model = model.to(device)
+    summary(model, (3, 224, 224))
+
+
+def train(model_path=None):
+    train_tf = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            (0.5, 0.5, 0.5),
+            (0.5, 0.5, 0.5)
+        )
+    ])
+    val_tf = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            (0.5, 0.5, 0.5),
+            (0.5, 0.5, 0.5)
+        )
+    ])
+    num_workers = 0
+    max_epoch = 1
+    accelerator = "cpu"
+    if platform.system().lower() == "linux":
+        max_epoch = 20
+        accelerator = "gpu"
+        num_workers = 8
+
+    train_set = datasets.CIFAR10(root="./data", train=True, download=True, transform=train_tf)
+    train_loader = DataLoader(train_set, batch_size=36, shuffle=True, num_workers=num_workers)
+    val_set = datasets.CIFAR10(root="./data", train=False, download=False, transform=val_tf)
+    val_loader = DataLoader(val_set, batch_size=5000, shuffle=False, num_workers=num_workers)
+
+    trainer = Trainer(max_epochs=max_epoch, accelerator=accelerator, log_every_n_steps=40)
+
+    classes = ("plane", 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'horse', 'ship', 'truck')
+    model = MobileNetV2()
+    if model_path is not None:
+        model.load_from_checkpoint(model_path)
+    trainer.fit(model, train_loader)
+    trainer.validate(model, val_loader)
